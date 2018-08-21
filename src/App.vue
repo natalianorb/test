8c8978fa4e02c635beeec61808762296128003a9
Natalia Norboeva <natanor@softomate.com>
2018-08-21 20:47:44 +0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <OrderTable :data="data"/>
+  </div>
+</template>
+
+<script>
+import OrderTable from './components/OrderTable'
+import data from './data1.json'
+
+export default {
+  name: 'App',
+  components: {
+    OrderTable
+  },
+  data: data
+}
+</script>
+
+<style>
+#app {
+}
+</style>
